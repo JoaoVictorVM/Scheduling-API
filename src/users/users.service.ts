cfc1c074df8d232ed934c446c2f79d38d923ad57
João Victor Ventura Martins <jvmartinscv@gmail.com>
2026-02-09 11:13:41 -0300

    feat(auth): hash user password with bcrypt

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async create(data: { name: string; email: string; password: string }) {
-    const user = await this.prisma.user.create({
-      data,
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    return this.prisma.user.create({
+      data: {
+        ...data,
+        password: hashedPassword,
+      },
     });
-
-    const { password, ...userWithoutPassword } = user;
-
-    return userWithoutPassword;
   }
 }
