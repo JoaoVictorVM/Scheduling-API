4aab0a97b598c3f4dd0ff521e90e30f99bf94554
João Victor Ventura Martins <jvmartinscv@gmail.com>
2026-02-19 21:39:59 -0300

    feat(auth): implementa login com JWT

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+@Module({
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: 'supersecret',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
