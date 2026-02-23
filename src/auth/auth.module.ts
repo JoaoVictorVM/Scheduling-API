a828534685d2bb3d00f063d24db5c820bc819404
João Victor Ventura Martins <jvmartinscv@gmail.com>
2026-02-23 14:28:12 -0300

    chore(config): move configuracoes JWT para secrets

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PrismaModule,
     PassportModule,
-    JwtModule.register({
-      secret: 'supersecret',
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): JwtModuleOptions => {
+        const secret = configService.getOrThrow<string>('JWT_SECRET');
+        const expiresIn = configService.getOrThrow<string>('JWT_EXPIRES_IN');
+
+        return {
+          secret,
+          signOptions: {
+            expiresIn: expiresIn as any,
+          },
+        };
+      },
     }),
   ],
   controllers: [AuthController],
